26b7efd4edb929cce83ba79d9fdd5e6c30d9bba9
BigDabettarat <157104576+BigDabettarat@users.noreply.github.com>
2025-01-28 18:01:58 +1100

    Add files via upload

---
+++ Demonology Console Print.lua
@@ -0,0 +1,19 @@
+local ghost = game.Workspace.Ghost
+
+print("Ghost Type:")
+print(ghost:GetAttribute("GhostType"))
+
+print("")
+
+print("Room Info")
+print("---------")
+
+print("")
+
+print("Current Room:")
+print(ghost:GetAttribute("CurrentRoom"))
+
+print("")
+
+print("Favorite Room:")
+print(ghost:GetAttribute("FavoriteRoom"))
